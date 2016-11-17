a7882c734551e479e9c921c80ede9d8ed1c79dc2
druchefavour <druchefavour@gmail.com>
2016-11-16 18:17:08 -0600

    commit to push in completed html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@
     questionsList = $(".question");
     var trivia = { answers: ['c', 'a', 'c', 'a', 'c', 'a', 'c', 'b', 'd', 'c'] },
     answers = trivia.answers,
-     questionLength= answers.length
+    questionLength= answers.length
 
     //Creating some helper functions
     function roundReloaded(num, dec) { 
@@ -66,6 +66,8 @@
     else if (seconds < 10 && seconds.length != 2) seconds = '0' + seconds;
         if (minutes < 10 && minutes.length != 2) minutes = '0' + minutes
         $('#spanTime').html(minutes + ':' + seconds); 
+    	if (parseInt(minutes, 02) == 0 && parseInt(seconds, 00) == 0)
+        clearInterval(interval);
 	}, 1000);
 
     $(this).parents('.questionContainer').fadeOut(500, function(){ 
@@ -73,5 +75,68 @@
     }); 
          return false; 
 	});
-  
-  })
+
+  	$('.btnPrev').click(function(){ 
+        notice.hide(); 
+    $(this).parents('.questionContainer').fadeOut(500, function(){ 
+        $(this).prev().fadeIn(500) 
+    }); 
+    progress.animate({ width: progress.width() - Math.round(progressWidth/questionLength), }, 500 ); 
+         return false; 
+	});
+	$('.btnNext').click(function(){ 
+        var tempCheck = $(this).parents('.questionContainer').find('input[type=radio]:checked'); 
+    if (tempCheck.length == 0) { 
+         notice.fadeIn(300);return false; 
+    } 
+         notice.hide(); 
+    $(this).parents('.questionContainer').fadeOut(500, function(){ 
+        $(this).next().fadeIn(500); 
+    }); 
+    progress.animate({ width: progress.width() + Math.round(progressWidth/questionLength), }, 500 ); 
+         return false; 
+});
+	$('.btnShowResult').click(function(){ 
+	// Stuff goes in here
+	var tempCheck = $(this).parents('.questionContainer').find('input[type=radio]:checked'); 
+	if (tempCheck.length == 0) { 
+     notice.fadeIn(300);return false; 
+	} 
+	var tempArr = $('input[type=radio]:checked'); 
+	for (var i = 0, ii = tempArr.length; i < ii; i++) { 
+    userAnswers.push(tempArr[i].getAttribute('data-key')); 
+    progressKeeper.hide(); 
+	var results = checkAnswers(),  
+              resultSet = '', 
+              trueCount = 0, 
+              answerKey = ' Answers <br />', 
+              score;
+	for (var i = 0, ii = results.length; i < ii; i++){ 
+    if (results[i] == true) trueCount++; 
+    resultSet += '<div class="resultRow"> Question #' + (i + 1) + (results[i]== true ? "<div class='correct'><span>Correct</span></div>": "<div class='wrong'><span>Wrong</span></div>") + "</div>"; 
+    answerKey += (i+1) +" : "+ answers[i] +' &nbsp;  &nbsp;  &nbsp;   '; 
+score =  roundReloaded(trueCount / questionLength*100, 2);
+answerKey = "<div id='answer-key'>" + answerKey + "</div>"; 
+resultSet = '<h2 class="qTitle">' +judgeSkills(score) + ' You scored '+score+'%</h2>' + resultSet + answerKey; 
+$('#resultKeeper').html(resultSet).show(); 
+     $(this).parents('.questionContainer').fadeOut(500, function(){ 
+    $(this).next().fadeIn(500); 
+}); 
+return false;
+} 
+	} 
+	});
+	
+
+
+
+progressKeeper.hide(); 
+notice.hide(); 
+$("#main-quiz-holder input:radio").attr("checked", false);
+ 
+$('.answers li input').click(function() { 
+    $(this).parents('.answers').children('li').removeClass("selected"); 
+    $(this).parents('li').addClass('selected'); 
+});
+
+ })
