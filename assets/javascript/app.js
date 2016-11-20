e33622850ffa8a17139a8119e2fc8b306f84a16c
druchefavour <druchefavour@gmail.com>
2016-11-19 18:22:26 -0600

    commit a fully functional game in draft form

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,42 @@
-
 // Initiate the trivia game
 $(document).ready(function() {
 
-//Declare all the variables to be used
+//======================================================
+//Declare the variables to be used
 var timer = $("#spanTime").html();
 var progress = $("#progress"),
 progressKeeper = $("#progressKeeper"),
 notice = $("#notice"),
 progressWidth = 548,
-userAnswers = [], 
+userAnswers = [],
 questionsStatus = $("#questionNumber"),
 questionsList = $(".question");
-var trivia = { answers: ['c', 'a', 'c', 'a', 'c', 'a', 'c', 'b', 'd', 'c'] },
+var trivia = { answers: ['c', 'a', 'c', 'a', 'c', 'a', 'c', 'b', 'd', 'a'] },
 answers = trivia.answers,
 questionLength = answers.length;
 
+//==========================================================
+// Create file to reset Game
+$('.resetButton').click(function () {
+    location.reload();
+})
+
+//==========================================================
 //Creating some helper functions
-    function roundReloaded(num, dec) {
+function roundReloaded(num, dec) {
     var result = Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec);
     return result;
-    }
-//making radio button such that user can select only one button at a time
+}
+
+//==============================================================
+//Creating function to make it possible for user to select only one radio button at a time
 $('input[type=radio]').prop('checked', false);
+$('input[type=radio]').on('change', function(){
+    $('input[type=radio]').not(this).prop('checked', false);
+});
 
-    $('input[type=radio]').on('change', function(){
-        $('input[type=radio]').not(this).prop('checked', false);
-    });
-
-//comment on final performance
+//===================================================================
+//Construct comments on final performance
 function finalPerformance(score) {
     var returnString;
     if (score === 100) {
@@ -37,7 +46,7 @@ function finalPerformance(score) {
     } else if (score > 70) {
         returnString = "Doff my hat!";
     } else if (score > 50) {
-        returnString = "mmmh!";
+        returnString = "Average Performance!";
     } else if (score > 35) {
         returnString = "Go back to school!";
     } else if (score > 20) {
@@ -47,6 +56,18 @@ function finalPerformance(score) {
         return returnString;
     }
 }
+
+//==================================
+function timeUp() {
+    $("#overlayTimeup").show();
+    $("#overlay-contentTimeup").show();
+    setTimeout(function() {
+        $("#overlayTimeup").fadeOut();
+    }, 5000);
+    $('#spanTime').html('00:00');
+}
+//================================
+
 // function to compare answers assigned with the users answer
 function checkAnswers() {
     var i;
@@ -62,7 +83,48 @@ function checkAnswers() {
         }
         return resultArr;
     }
-
+//======================================================================
+//function to calculate final scores
+function calculate() {
+    progressKeeper.hide();
+    var results = checkAnswers(),
+    resultSet = '',
+    trueCount = 0,
+    answerKey = ' Answers <br>',
+    score;
+    for (var i = 0, ii = results.length; i < ii; i++) {
+        if (results[i] == true) trueCount++;
+        resultSet += '<div class="resultRow"> Question #'+ (i + 1) + (results[i] == true ? "<div class='correct'><span>Correct</span></div>": "<div class='wrong'><span>Wrong</span></div>") + "</div>";
+        answerKey += (i+1) +" : "+ answers[i] + ' &nbsp;  &nbsp;  &nbsp;   ';
+    }
+    score = roundReloaded(trueCount / questionLength * 100, 2);
+    answerKey += "<div id='answer-key'>" + answerKey + "</div>";
+    resultSet = '<h2 class="qTitle">' +  finalPerformance(score) + ' You scored '+ score +'%</h2>' + resultSet + answerKey;
+    $('#resultKeeper').html(resultSet).show();
+    $(this).parents('.questionContainer').fadeOut(500, function () {
+        $(this).next().fadeIn(500);
+    });
+    return false;
+    progressKeeper.hide();
+    notice.hide();
+    $("#main-question-holder input:radio").attr("checked", false)
+    $('.answers li input').click(function() {
+        $(this).parents('.answers').children('li').removeClass("selected");
+        $(this).parents('li').addClass('selected');
+    });
+}
+//========================================================================
+//function to stop timer when game is over
+function stopTimer(){
+    var gameTimer;
+    gameTimer = setInterval()
+ if (userAnswers.length === questionLength){
+    clearInterval(gateTimer);
+ }   
+}
+    
+//=================================================================
+//Click the start image to start the game
 $('.startClick').click(function () {
     setInterval(function () {
         var timer = $('#spanTime').html(); // Write the timer on the html page
@@ -70,110 +132,81 @@ $('.startClick').click(function () {
         var minutes = parseInt(timer[0], 10);
         var seconds = parseInt(timer[1], 10);
         seconds -= 1;
-        if (minutes < 0) return clearInterval(interval);
-            if (minutes < 10 && minutes.length != 2) minutes = '0' + minutes;
-            if (seconds < 0 && minutes != 0) {
-                minutes -= 1;
-                seconds = 59;
-            } else if (seconds < 10 && seconds.length != 2)
-            seconds = '0' + seconds;
-            $('#spanTime').html(minutes + ':' + seconds);
-            if (minutes == 0 && seconds == 0) clearInterval();
-        }, 1000);      
-        $(this).parents('.questionContainer').fadeOut(500, function() { 
+        if ((minutes < 0) && (seconds <= 0)) return clearInterval(timer);
+        if (minutes < 10 && minutes.length != 2) minutes = '0' + minutes;
+        if (seconds < 0 && minutes != 0) {
+            minutes -= 1;
+            seconds = 59;
+            seconds >=0;
+        } else if (seconds < 10 && seconds.length != 2)
+        seconds = '0' + seconds;
+        $('#spanTime').html(minutes + ':' + seconds);
+        if (minutes == 0 && seconds == 0) clearInterval();
+    }, 1000);
+    $(this).parents('.questionContainer').fadeOut(500, function() { 
         $(this).next().fadeIn(500, function(){ progressKeeper.show();
         });
-        return false; 
-});
-//function to set time for the quiz
-setTimeout(timeUp, 1000 * 2 * 60);
-function timeUp() {
-     $('#spanTime').html('00:00');
-   // $('#spanTime').html(Time is Up);
-    alert('Your Time is Up');
-}
-
-
-$('.btnPrev').click(function (){
-    notice.hide();
-    $(this).parents('.questionContainer').fadeOut(500, function () {
-        $(this).prev().fadeIn(500)
+        setTimeout(timeUp, 1000 * 2 * 60);
+        return false;
     });
-progress.animate({ 
-    width: progress.width() - Math.round(progressWidth / questionLength), 
-}, 500 );
-return false;
-});
-	
-$('.btnNext').click(function (){
-    var tempCheck = $(this).parents('.questionContainer').find('input[type=radio]:checked');
-    if (tempCheck.length == 0) {
-        notice.fadeIn(300);
-        return false;    
-    }
+    //Create click function to automatically transit from one question to the other
+$('input[type="radio"]').change(function(){
     var tempArr = $('input[type=radio]:checked');
-    //alert($("input[type=radio]:checked").size())
-    //alert(tempArr.length);
     for (var i = 0, ii = tempArr.length; i < ii; i++) {
         userAnswers.push(tempArr[i].getAttribute('data-key'));
-        //alert(answers[userAnswers.length-1l.]);
-        }//var answervalue = [];
-    //for (var i; i = 0; i < answers.length, i++);
-            //answervalue.push(answers[i]);
-        if ((answers[userAnswers.length-1] === userAnswers[userAnswers.length-1])) {
-        alert("Correct!");
-    } else {
-        alert("Your Answer is Wrong " + "The correct Answer is " + answers[userAnswers.length-1])
     }
-
-    notice.hide();
-    $(this).parents('.questionContainer').fadeOut(500, function (){
-        $(this).next().fadeIn(500);
-    }); 
-    progress.animate({ width: progress.width() + Math.round(progressWidth/questionLength), }, 500 ); 
-         return false;
-     });
-});
-
-$('.btnShowResult').click(function () {
-    var tempCheck = $(this).parents('.questionContainer').find('input[type=radio]:checked');
-    if (tempCheck.length == 0) {
-        notice.fadeIn(300);
+    calculate();
+    //stopTimer();
+    if (answers[userAnswers.length-1] == userAnswers[userAnswers.length-1]) {
+        $.fn.center = function () {
+            this.css("position", "absolute");
+            this.css("top", Math.max(0, (
+                ($(window).height() - $(this).outerHeight()) / 2) + 
+            $(window).scrollTop()) + "px" );
+            this.css("left", Math.max(0,
+                ( ($(window).width() - $(this). outerWidth()) / 2) + 
+                $(window).scrollLeft()) + "px" );
+            return this;
+        }
+        $("#overlay").show();
+        $("#overlay-content").show().center();
+        setTimeout(function() {
+            $("#overlay").fadeOut();
+        }, 5000);
+        notice.hide();
+        $(this).parents('.questionContainer').fadeOut(500, function () {
+            $(this).next().fadeIn(500);
+        });
+        progress.animate({
+            width: progress.width() + Math.round(progressWidth/questionLength), }, 500 );
         return false;
-    }
-    //var tempArr = $('input[type=radio]:checked');
-    //alert($("input[type=radio]:checked").size())
-    //alert(tempArr.length);
-    //for (var i = 0, ii = tempArr.length; i < ii; i++) {
-    //    userAnswers.push(tempArr[i].getAttribute('data-key'));
-    //    alert(userAnswers);
-  //  }
-        progressKeeper.hide();
-        var results = checkAnswers(),
-        resultSet = '',
-        trueCount = 0,
-        answerKey = ' Answers <br>',
-        score;
-            for (var i = 0, ii = results.length; i < ii; i++){
-            if (results[i] == true) trueCount++;
-            alert(trueCount);
-            resultSet += '<div class="resultRow"> Question #' + (i + 1) + (results[i] == true ? "<div class='correct'><span>Correct</span></div>": "<div class='wrong'><span>Wrong</span></div>") + "</div>";
-            answerKey += (i+1) +" : "+ answers[i] //+' &nbsp;  &nbsp;  &nbsp;   '; //&nbsp is non-breaking space
-            }
-            score =  roundReloaded(trueCount / questionLength * 100, 2);
-            answerKey = "<div id='answer-key'>" + answerKey + "</div>";
-            resultSet = '<h2 class="qTitle">' +  finalPerformance(score) + ' You scored '+ score +'%</h2>' + resultSet + answerKey;
-            $('#resultKeeper').html(resultSet).show();
-            $(this).parents('.questionContainer').fadeOut(500, function(){
-                $(this).next().fadeIn(500);
-            });
-            return false;
-progressKeeper.hide();
-notice.hide();
-$("#main-question-holder input:radio").attr("checked", false);
-$('.answers li input').click(function () {
-    $(this).parents('.answers').children('li').removeClass("selected");
-    $(this).parents('li').addClass('selected'); 
+        
+    } else {
+        $.fn.center = function () {
+            this.css("position", "absolute");
+            this.css("top", Math.max(0, (
+                ($(window).height() - $(this).outerHeight()) / 2) + 
+            $(window).scrollTop()) + "px" );
+            this.css("left", Math.max(0,
+                ( ($(window).width() - $(this). outerWidth()) / 2) + 
+                $(window).scrollLeft()) + "px" );
+            return this;
+        }
+        $("#overlaywrong").show();
+        $("#overlay-content-wrong").show().center();
+        setTimeout(function() {
+            $("#overlaywrong").fadeOut();
+        }, 5000);
+        notice.hide();
+        $(this).parents('.questionContainer').fadeOut(500, function () {
+            $(this).next().fadeIn(500);
+        });
+        progress.animate({
+            width: progress.width() + Math.round(progressWidth/questionLength), }, 500 );
+        return false;
+};
 });
 });
-})
+});
+
+//======================================================================================
