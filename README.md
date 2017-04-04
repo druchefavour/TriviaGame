e6a3f3c461adc49da6694fa391d3eee722395267
druchefavour <druchefavour@gmail.com>
2017-04-04 11:14:13 -0500

    commit readme

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# TriviaGame
+# Trivia Game
+
+Overview
+
+In this project a Trivia game using JavaScript for the logic and jQuery to manipulate HTML is created. The app is layed out with valid HTML and stylish CSS.
+
+The trivia game shows only one question until the player answers it or their time runs out. In the app, a trivia form is created with multiple choice.
+
+The player is given fifty seconds to finish the quiz.
+
+The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly. The player is not allowed to pick more than one answer per question. A countdown timer is included in the app.
+
+
+## Procedure
+
+* Create a GitHub repo called TriviaGame, then clone the repo to the local computer.
+
+* Create a file inside of the TriviaGame folder called ```index.html```. In this file all of the  HTML would be marked up.
+
+* Add/include a script tag with the jQuery library.
+
+* Create a folder inside of the TriviaGame folder called assets.
+  * Inside assets, create three folders: css, javascript, images
+    * In the css folder, create a style.css file.
+    * In the javascript folder, create an app.js file; here all of the JavaScript and jQuery scripts would e written.
+    * In the images folder, all the images used in creating the app is saved.
+
+
+## How the app works
+* If the player selects the correct answer, a screen congratulating them for choosing the right option is shown. After five seconds the next question is displayed automatically.
+
+* The scenario is similar for wrong answers and time-outs.
+
+* If the player runs out of time, a modal appears and the player is notified that time's up. Then the correct answer is displayed. The app then waits a for five seconds and shows the next question.
+
+* If the player chooses the wrong answer, the app notifies the player they selected the wrong option and then displays the correct answer. Then the app waits a for five seconds, and then shows the next question.
+
+* On the final screen, the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page) is shown.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,48 @@
+
 // Initiate the trivia game
 $(document).ready(function() {
 
-//======================================================
-//Declare the variables to be used
+//Declare all the variables to be used
 var timer = $("#spanTime").html();
 var progress = $("#progress"),
 progressKeeper = $("#progressKeeper"),
 notice = $("#notice"),
 progressWidth = 548,
-userAnswers = [],
+userAnswers = [], 
 questionsStatus = $("#questionNumber"),
 questionsList = $(".question");
-var trivia = { answers: ['c', 'a', 'c', 'a', 'c', 'a', 'c', 'b', 'd', 'a'] },
+var trivia = { answers: ['c', 'a', 'c', 'a', 'c', 'a', 'c', 'b', 'd', 'c'] },
 answers = trivia.answers,
 questionLength = answers.length;
 
-//==========================================================
-// Create file to reset Game
-$('.resetButton').click(function () {
-    location.reload();
-})
-
-//==========================================================
 //Creating some helper functions
-function roundReloaded(num, dec) {
+    function roundReloaded(num, dec) {
     var result = Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec);
     return result;
-}
+    }
 
-//==============================================================
-//Creating function to make it possible for user to select only one radio button at a time
-$('input[type=radio]').prop('checked', false);
-$('input[type=radio]').on('change', function(){
-    $('input[type=radio]').not(this).prop('checked', false);
-});
-
-//==================================
-function timeUp() {
-    $("#overlayTimeup").show();
-    $("#overlay-contentTimeup").show();
-    setTimeout(function() {
-        $("#overlayTimeup").fadeOut();
-    }, 5000);
-    $('#spanTime').html('00:00');
+//comment on final performance
+//if statements will work without the braces as long as there is only one return line in the string
+function finalPerformance(score) {
+    var returnString;
+    if (score === 100) {
+        returnString = "Awesome!";
+    } else if (score > 90) {
+        returnString = "Great!";
+    } else if (score > 70) {
+        returnString = "Doff my hat!";
+    } else if (score > 50) {
+        returnString = "mmmh!";
+    } else if (score > 35) {
+        returnString = "Go back to school!";
+    } else if (score > 20) {
+        returnString = "Go do something else!";
+    } else {
+        returnString = "We need to talk!";
+       
+    }
+    return returnString;
 }
-//================================
 
 // function to compare answers assigned with the users answer
 function checkAnswers() {
@@ -61,145 +59,110 @@ function checkAnswers() {
         }
         return resultArr;
     }
-//======================================================================
-//===================================================================
-//Construct comments on final performance
-function finalPerformance(score) {
-    var returnString;
-    if (score === 100) returnString = "Awesome!";
-    else if ((score > 90) && (score < 100)) returnString = "Great!";
-    else if ((score > 70) && (score <= 90)) returnString = "Doff my hat!";
-    else if ((score > 50) && (score <= 70)) returnString = "mmmh!";
-    else if ((score > 35) && (score <= 50)) returnString = "Go back to school!";
-    else if ((score > 20) && (score <=  35)) returnString = "Go do something else!";
-    else returnString = "We need to talk!";
-    return returnString;
-}
-//======================================================================
-//function to calculate final scores
-function calculate() {
-    progressKeeper.hide();
-    var results = checkAnswers(),
-    resultSet = '',
-    trueCount = 0,
-    answerKey = ' Answers <br>',
-    score;
-    for (var i = 0, ii = results.length; i < ii; i++) {
-        if (results[i] == true) trueCount++;
-        resultSet += '<div class="resultRow"> Question #'+ (i + 1) + (results[i] == true ? "<div class='correct'><span>Correct</span></div>": "<div class='wrong'><span>Wrong</span></div>") + "</div>";
-        answerKey += (i+1) +" : "+ answers[i] + ' &nbsp;  &nbsp;  &nbsp;   ';
-    }
-    score = roundReloaded(trueCount / questionLength * 100, 2);
-    answerKey += "<div id='answer-key'>" + answerKey + "</div>";
-    
-    resultSet = '<h2 class="resultTitle">' +  finalPerformance(score) + ' You scored '+ score +'%</h2>' + resultSet + answerKey;
-    $('#resultKeeper').html(resultSet).show();
-    $(this).parents('.questionContainer').fadeOut(500, function () {
-        $(this).next().fadeIn(500);
-    });
-    return false;
-    progressKeeper.hide();
-    notice.hide();
-    $("#main-question-holder input:radio").attr("checked", false)
-    $('.answers li input').click(function() {
-        $(this).parents('.answers').children('li').removeClass("selected");
-        $(this).parents('li').addClass('selected');
-    });
-}
-//========================================================================
-//function to stop timer when game is over
-function stopTimer(){
-    var gameTimer;
-    gameTimer = setInterval()
- if (userAnswers.length === questionLength){
-    clearInterval(gateTimer);
- }   
-}
-    
-//=================================================================
-//Click the start image to start the game
+
 $('.startClick').click(function () {
-    setInterval(function () {
+    var that = this
+    var interval = setInterval(function () {
         var timer = $('#spanTime').html(); // Write the timer on the html page
         timer = timer.split(':'); // convert the timer string to an array holding minutes and seconds
         var minutes = parseInt(timer[0], 10);
         var seconds = parseInt(timer[1], 10);
         seconds -= 1;
-        if ((minutes < 0) && (seconds <= 0)) return clearInterval(timer);
-        if (minutes < 10 && minutes.length != 2) minutes = '0' + minutes;
-        if (seconds < 0 && minutes != 0) {
-            minutes -= 1;
-            seconds = 59;
-            seconds >=0;
-        } else if (seconds < 10 && seconds.length != 2)
-        seconds = '0' + seconds;
-        $('#spanTime').html(minutes + ':' + seconds);
-        if (minutes == 0 && seconds == 0) clearInterval();
-    }, 1000);
-    $(this).parents('.questionContainer').fadeOut(500, function() { 
-        $(this).next().fadeIn(500, function(){ progressKeeper.show();
+        if (minutes < 0) return clearInterval(interval);
+            if (minutes < 10 && minutes.length != 2) minutes = '0' + minutes;
+            if (seconds < 0 && minutes != 0) {
+                minutes -= 1;
+                seconds = 59;
+            } else if (seconds < 10 && seconds.length != 2)
+            seconds = '0' + seconds;
+            $('#spanTime').html(minutes + ':' + seconds);
+            if (minutes == 0 && seconds == 0) clearInterval(interval);
+        }, 1000);  
+
+        //reassign this to another variable name. it is not showing up because it is inside. use that for this for instance    
+        $(that).parents('.questionContainer').fadeOut(500, function() { 
+        $(that).next().fadeIn(500, function(){ progressKeeper.show();
         });
-        setTimeout(timeUp, 1000 * 2 * 60);
-        return false;
+        return false; 
+});
+
+setTimeout(timeUp, 1000 * 2 * 60);
+function timeUp() {
+     $('#spanTime').html('00:00');
+   // $('#spanTime').html(Time is Up);
+    alert('Your Time is Up');
+}
+
+
+$('.btnPrev').click(function (){
+    notice.hide();
+    $(this).parents('.questionContainer').fadeOut(500, function () {
+        $(this).prev().fadeIn(500)
     });
-    //Create click function to automatically transit from one question to the other
-$('input[type="radio"]').change(function(){
+progress.animate({ 
+    width: progress.width() - Math.round(progressWidth / questionLength), 
+}, 500 );
+return false;
+});
+	
+$('.btnNext').click(function (){
+    var tempCheck = $(this).parents('.questionContainer').find('input[type=radio]:checked');
+    if (tempCheck.length == 0) {
+        notice.fadeIn(300);
+        return false;    
+    }
+    //var answervalue = [];
+    for (var i; i = 0; i < answers.length, i++);
+            //answervalue.push(answers[i]);
+        if ((answers[i] !== userAnswers[i])) {
+        alert("Your answer is wrong " + "The correct answer is " + answers[i]);
+    }
+
+    notice.hide();
+    $(this).parents('.questionContainer').fadeOut(500, function (){
+        $(this).next().fadeIn(500);
+    }); 
+    progress.animate({ width: progress.width() + Math.round(progressWidth/questionLength), }, 500 ); 
+         return false;
+     });
+});
+
+$('.btnShowResult').click(function () {
+    var tempCheck = $(this).parents('.questionContainer').find('input[type=radio]:checked');
+    if (tempCheck.length == 0) {
+        notice.fadeIn(300);
+        return false;
+    }
     var tempArr = $('input[type=radio]:checked');
     for (var i = 0, ii = tempArr.length; i < ii; i++) {
         userAnswers.push(tempArr[i].getAttribute('data-key'));
+        progressKeeper.hide();
+        var results = checkAnswers(),
+        resultSet = '',
+        trueCount = 0,
+        answerKey = ' Answers <br>',
+        score;
+        for (var i = 0, ii = results.length; i < ii; i++){
+            if (results[i] == true) trueCount++;
+            resultSet += '<div class="resultRow"> Question #' + (i + 1) + (results[i] == true ? "<div class='correct'><span>Correct</span></div>": "<div class='wrong'><span>Wrong</span></div>") + "</div>";
+            answerKey += (i+1) +" : "+ answers[i] +' &nbsp;  &nbsp;  &nbsp;   '; //&nbsp is non-breaking space
+            score =  roundReloaded(trueCount / questionLength * 100, 2);
+            answerKey = "<div id='answer-key'>" + answerKey + "</div>";
+            resultSet = '<h2 class="qTitle">' +  finalPerformance(score) + ' You scored '+ score +'%</h2>' + resultSet + answerKey;
+            $('#resultKeeper').html(resultSet).show();
+            $(this).parents('.questionContainer').fadeOut(500, function(){
+                $(this).next().fadeIn(500);
+            });
+            return false;
+        }
     }
-    calculate();
-    //stopTimer();
-    if (answers[userAnswers.length-1] == userAnswers[userAnswers.length-1]) {
-        $.fn.center = function () {
-            this.css("position", "absolute");
-            this.css("top", Math.max(0, (
-                ($(window).height() - $(this).outerHeight()) / 2) + 
-            $(window).scrollTop()) + "px" );
-            this.css("left", Math.max(0,
-                ( ($(window).width() - $(this). outerWidth()) / 2) + 
-                $(window).scrollLeft()) + "px" );
-            return this;
-        }
-        $("#overlay").show();
-        $("#overlay-content").show().center();
-        setTimeout(function() {
-            $("#overlay").fadeOut();
-        }, 5000);
-        notice.hide();
-        $(this).parents('.questionContainer').fadeOut(500, function () {
-            $(this).next().fadeIn(500);
-        });
-        progress.animate({
-            width: progress.width() + Math.round(progressWidth/questionLength), }, 500 );
-        return false;
-        
-    } else {
-        $.fn.center = function () {
-            this.css("position", "absolute");
-            this.css("top", Math.max(0, (
-                ($(window).height() - $(this).outerHeight()) / 2) + 
-            $(window).scrollTop()) + "px" );
-            this.css("left", Math.max(0,
-                ( ($(window).width() - $(this). outerWidth()) / 2) + 
-                $(window).scrollLeft()) + "px" );
-            return this;
-        }
-        $("#overlaywrong").show();
-        $("#overlay-content-wrong").show().center();
-        setTimeout(function() {
-            $("#overlaywrong").fadeOut();
-        }, 5000);
-        notice.hide();
-        $(this).parents('.questionContainer').fadeOut(500, function () {
-            $(this).next().fadeIn(500);
-        });
-        progress.animate({
-            width: progress.width() + Math.round(progressWidth/questionLength), }, 500 );
-        return false;
-};
-});
-});
 });
 
-//======================================================================================
+progressKeeper.hide();
+notice.hide();
+$("#main-question-holder input:radio").attr("checked", false);
+$('.answers li input').click(function () {
+    $(this).parents('.answers').children('li').removeClass("selected");
+    $(this).parents('li').addClass('selected'); 
+});
+})
